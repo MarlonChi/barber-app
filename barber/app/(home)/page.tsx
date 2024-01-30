dd98c662239cdf1e720d77886e36da13151c477a
MarlonChi <marlonchiodelli@hotmail.com>
2024-01-30 11:51:36 -0300

    feat: add booking component

--- barber/app/(home)/page.tsx
+++ barber/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { ptBR } from "date-fns/locale";
 
 import Header from "../_components/Header";
 import Search from "./_components/Search";
+import BookingItem from "../_components/BookingItem";
 
 export default function Home() {
   return (
@@ -20,6 +21,13 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+        <BookingItem />
       </div>
     </div>
   );
