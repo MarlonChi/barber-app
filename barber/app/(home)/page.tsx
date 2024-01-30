9d81845e4049f8e440c57126ef88771252517f1a
MarlonChi <marlonchiodelli@hotmail.com>
2024-01-30 11:32:06 -0300

    feat: add search component

--- barber/app/(home)/page.tsx
+++ barber/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 import Header from "../_components/Header";
+import Search from "./_components/Search";
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
